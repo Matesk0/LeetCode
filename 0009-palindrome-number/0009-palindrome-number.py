13f091268978a955a0f73c7cfb6f67433f7a161b
Matesk0 <matiaskojakub17@gmail.com>
2025-08-26 13:03:36 +0200

    Time: 6 ms (79.88%), Space: 12.4 MB (81.34%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        r_half = 0
+        while x > r_half:
+            r_half = r_half * 10 + x % 10
+            x //= 10
+
+        return x == r_half or x == r_half // 10
