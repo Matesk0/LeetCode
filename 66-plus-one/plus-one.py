ec9ce41b739970a0afb5c893411404cf6a5ffef6
Matesk0 <matiaskojakub17@gmail.com>
2025-09-22 19:46:36 +0200

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.85%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        pos = 0
+
+        for idx in range(len(digits) - 1, -1, -1):
+            if digits[idx] == 9:
+                digits[idx] = 0
+                continue
+            
+            digits[idx] += 1
+            break
+        
+        if digits[0] == 0:
+            digits.insert(0, 1)
+        
+        return digits
