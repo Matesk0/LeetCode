c44d4ba374d8f78bebef516497cdd5005c6ae775
Matesk0 <matiaskojakub17@gmail.com>
2025-09-25 13:36:55 +0200

    Time: 11 ms (94.73%) | Memory: 12.7 MB (68.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,22 +8,18 @@ class Solution(object):
         letters = {}
 
         for letter in s:
-            if letter in letters.keys():
+            if letter in letters:
                 letters[letter] += 1
-            
+
             else:
                 letters[letter] = 1
         
         for letter in t:
-            check = letters.get(letter, None)
-
-            if check is None:
+            if letter in letters:
+                letters[letter] -= 1
+            
+            else:
                 return False
-
-            letters[letter] -= 1
-
-            if letters[letter] == 0:
-                letters.pop(letter)
-
-        return not letters
+        
+        return all(letters[letter] == 0 for letter in letters)
         
