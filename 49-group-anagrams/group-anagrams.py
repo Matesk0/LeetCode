0581c2fc55f52a49d2bd7a4b812bfc915ba7b2cb
Matesk0 <matiaskojakub17@gmail.com>
2025-09-25 13:54:42 +0200

    Time: 19 ms (80.16%) | Memory: 15.7 MB (99.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,26 +5,13 @@ class Solution(object):
         :rtype: List[List[str]]
         """
 
-        all_letters = {}
-        result = []
-        index = 0
+        result = {}
 
         for word in strs:
             letters = {}
 
-            for l in word:
-                letters[l] = letters.get(l, 0) + 1
+            key = ''.join(sorted(word))
 
-            key = tuple(sorted(letters.items()))
-            
-            check_idx = all_letters.get(key, None)
-
-            if check_idx is None:
-                all_letters[key] = index
-                result.append([word])
-                index += 1
-            
-            else:
-                result[check_idx].append(word)
+            result[key] = result.get(key, []) + [word]
         
-        return result
+        return result.values()
